d0b7c7b095f13a9175995c3dcc785dcaee791141
superctj <congtj@umich.edu>
2024-06-16 17:28:48 -0400

    add unit tests for inferring table embeddings

--- tests/test_bert_embedding_inference.py
+++ tests/test_bert_embedding_inference.py
@@ -7,6 +7,7 @@ from observatory.datasets.wikitables import WikiTables
 from observatory.models.bert import BERTModelWrapper
 from observatory.preprocessing.columnwise import ColumnwiseMaxRowsPreprocessor
 from observatory.preprocessing.rowwise import RowwiseMaxRowsPreprocessor
+from observatory.preprocessing.tablewise import TableMaxRowsPreprocessor
 
 
 class TestBERTEmbeddingInference(unittest.TestCase):
@@ -29,10 +30,14 @@ class TestBERTEmbeddingInference(unittest.TestCase):
             tokenizer=self.model_wrapper.tokenizer,
             max_input_size=self.model_wrapper.max_input_size,
         )
+        self.table_max_rows_preprocessor = TableMaxRowsPreprocessor(
+            tokenizer=self.model_wrapper.tokenizer,
+            max_input_size=self.model_wrapper.max_input_size,
+        )
 
     def test_infer_column_embeddings_from_wikitables(self):
         truncated_tables = (
-            self.columnwise_max_rows_preprocessor.columnwise_truncation(
+            self.columnwise_max_rows_preprocessor.truncate_columnwise(
                 self.wikitables_dataset.all_tables
             )
         )
@@ -42,15 +47,31 @@ class TestBERTEmbeddingInference(unittest.TestCase):
         assert len(column_embeddings) == len(self.wikitables_dataset.all_tables)
 
     def test_infer_row_embeddings_from_wikitables(self):
-        truncated_tables = (
-            self.rowwise_max_rows_preprocessor.rowwise_truncation(
-                self.wikitables_dataset.all_tables
-            )
+        truncated_tables = self.rowwise_max_rows_preprocessor.truncate_rowwise(
+            self.wikitables_dataset.all_tables
         )
         row_embeddings = self.model_wrapper.infer_row_embeddings(
             truncated_tables, batch_size=32
         )
         assert len(row_embeddings) == len(self.wikitables_dataset.all_tables)
+
+    def test_infer_table_embeddings_from_wikitables(self):
+        # Serialize tables by column to infer table embeddings
+        truncated_tables = self.table_max_rows_preprocessor.truncate_columnwise(
+            self.wikitables_dataset.all_tables
+        )
+        table_embeddings = self.model_wrapper.infer_table_embeddings(
+            truncated_tables, serialize_by_row=False, batch_size=32
+        )
+        assert len(table_embeddings) == len(self.wikitables_dataset.all_tables)
+
+        # Serialize tables by row to infer table embeddings
+        truncated_tables = self.table_max_rows_preprocessor.truncate_rowwise(
+            self.wikitables_dataset.all_tables
+        )
+        table_embeddings = self.model_wrapper.infer_table_embeddings(
+            truncated_tables, serialize_by_row=True, batch_size=32
+        )
 
     def test_infer_column_embeddings_from_sotab(self):
         pass
