33da2ae232d7c4607aafea9a1e7e19c2c9b5097e
superctj <congtj@umich.edu>
2024-06-17 02:15:58 -0400

    add unit-test for cell embedding inference

--- tests/test_bert_embedding_inference.py
+++ tests/test_bert_embedding_inference.py
@@ -1,10 +1,12 @@
 import os
 import unittest
 
+import pandas as pd
 import torch
 
 from observatory.datasets.wikitables import WikiTables
 from observatory.models.bert import BERTModelWrapper
+from observatory.preprocessing.cellwise import CellMaxRowsPreprocessor
 from observatory.preprocessing.columnwise import ColumnwiseMaxRowsPreprocessor
 from observatory.preprocessing.rowwise import RowwiseMaxRowsPreprocessor
 from observatory.preprocessing.tablewise import TableMaxRowsPreprocessor
@@ -22,15 +24,26 @@ class TestBERTEmbeddingInference(unittest.TestCase):
         self.wikitables_dataset = WikiTables(data_dir)
         self.model_wrapper = BERTModelWrapper(model_name, device)
 
+        # Prprocessor for inferring column embeddings
         self.columnwise_max_rows_preprocessor = ColumnwiseMaxRowsPreprocessor(
             tokenizer=self.model_wrapper.tokenizer,
             max_input_size=self.model_wrapper.max_input_size,
         )
+
+        # Preprocessor for inferring row embeddings
         self.rowwise_max_rows_preprocessor = RowwiseMaxRowsPreprocessor(
             tokenizer=self.model_wrapper.tokenizer,
             max_input_size=self.model_wrapper.max_input_size,
         )
+
+        # Preprocessor for inferring table embeddings
         self.table_max_rows_preprocessor = TableMaxRowsPreprocessor(
+            tokenizer=self.model_wrapper.tokenizer,
+            max_input_size=self.model_wrapper.max_input_size,
+        )
+
+        # Preprocessor for inferring cell embeddings
+        self.cell_max_rows_preprocessor = CellMaxRowsPreprocessor(
             tokenizer=self.model_wrapper.tokenizer,
             max_input_size=self.model_wrapper.max_input_size,
         )
@@ -72,6 +85,80 @@ class TestBERTEmbeddingInference(unittest.TestCase):
         table_embeddings = self.model_wrapper.infer_table_embeddings(
             truncated_tables, serialize_by_row=True, batch_size=32
         )
+        assert len(table_embeddings) == len(self.wikitables_dataset.all_tables)
 
-    def test_infer_column_embeddings_from_sotab(self):
-        pass
+    def test_infer_cell_embeddings_from_wikitables(self):
+        def assert_num_cells_matches_num_embeddings(
+            tables: list[pd.DataFrame], cell_embeddings: list[torch.Tensor]
+        ):
+            """Assert the number of cells in each table matches the number of
+            cell embeddings."""
+            for i, tbl in enumerate(tables):
+                num_cells = tbl.shape[0] * tbl.shape[1]
+                assert len(cell_embeddings[i]) == num_cells
+
+        # Include headers in serialized tables
+        include_headers = True
+
+        # Serialize tables by column to infer cell embeddings
+        truncated_tables = self.cell_max_rows_preprocessor.truncate_columnwise(
+            self.wikitables_dataset.all_tables, include_headers
+        )
+        cell_embeddings = self.model_wrapper.infer_cell_embeddings(
+            truncated_tables,
+            serialize_by_row=False,
+            include_headers=include_headers,
+            batch_size=32,
+        )
+
+        assert_num_cells_matches_num_embeddings(
+            truncated_tables, cell_embeddings
+        )
+
+        # Serialize tables by row to infer cell embeddings
+        truncated_tables = self.cell_max_rows_preprocessor.truncate_rowwise(
+            self.wikitables_dataset.all_tables, include_headers
+        )
+        cell_embeddings = self.model_wrapper.infer_cell_embeddings(
+            truncated_tables,
+            serialize_by_row=True,
+            include_headers=include_headers,
+            batch_size=32,
+        )
+
+        assert_num_cells_matches_num_embeddings(
+            truncated_tables, cell_embeddings
+        )
+
+        # Do not include headers in serialized tables
+        include_headers = False
+
+        # Serialize tables by column to infer cell embeddings
+        truncated_tables = self.cell_max_rows_preprocessor.truncate_columnwise(
+            self.wikitables_dataset.all_tables, include_headers
+        )
+        cell_embeddings = self.model_wrapper.infer_cell_embeddings(
+            truncated_tables,
+            serialize_by_row=False,
+            include_headers=include_headers,
+            batch_size=32,
+        )
+
+        assert_num_cells_matches_num_embeddings(
+            truncated_tables, cell_embeddings
+        )
+
+        # Serialize tables by row to infer cell embeddings
+        truncated_tables = self.cell_max_rows_preprocessor.truncate_rowwise(
+            self.wikitables_dataset.all_tables, include_headers
+        )
+        cell_embeddings = self.model_wrapper.infer_cell_embeddings(
+            truncated_tables,
+            serialize_by_row=True,
+            include_headers=include_headers,
+            batch_size=32,
+        )
+
+        assert_num_cells_matches_num_embeddings(
+            truncated_tables, cell_embeddings
+        )
