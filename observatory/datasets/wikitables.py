b5264347201002139ce0f45212cb58c5491f0150
superctj <congtj@umich.edu>
2024-06-23 18:36:15 -0400

    implement wikitables dataset and dataloader on top of pytorch dataset and dataloader

--- observatory/datasets/wikitables.py
+++ observatory/datasets/wikitables.py
@@ -2,23 +2,49 @@ import os
 
 import pandas as pd
 
+from torch.utils.data import Dataset
 
-class WikiTables:
-    def __init__(self, data_dir: str):
+
+class WikiTablesDataset(Dataset):
+    def __init__(
+        self, data_dir: str, metadata_filepath: str, transform: callable = None
+    ):
+        """
+        Args:
+            data_dir:
+                The directory containing tables.
+            metadata_filepath:
+                The path to the metadata file, which contains table file names
+                and potentially other information of tables.
+        """
+
         self.data_dir = data_dir
-        self.all_tables = self._get_tables()
+        self.metadata = pd.read_csv(metadata_filepath)
+        self.transform = transform
 
-    def _get_tables(self):
-        all_tables = []
+    def __len__(self):
+        return self.metadata.shape[0]
 
-        for f in os.listdir(self.data_dir):
-            if f.endswith(".csv"):
-                table = pd.read_csv(os.path.join(self.data_dir, f))
+    def __getitem__(self, idx) -> pd.DataFrame:
+        # Assume the first column of the metadata file contains table file names
+        table_filepath = os.path.join(self.data_dir, self.metadata.iloc[idx, 0])
+        table = pd.read_csv(table_filepath)
 
-                table.columns = table.columns.astype(str)
-                table = table.reset_index(drop=True)
-                table = table.astype(str)
+        # Add table name
+        table.attrs["name"] = self.metadata.iloc[idx, 0]
 
-                all_tables.append(table)
+        # Drop NaN columns and rows
+        table.dropna(axis=1, how="all", inplace=True)
+        table.dropna(axis=0, how="all", inplace=True)
 
-        return all_tables
+        # Convert all columns to string
+        table = table.astype(str)
+
+        if self.transform:
+            table = self.transform(table)
+
+        return table
+
+
+def collate_fn(batch: list[pd.DataFrame]):
+    return batch
