d2fa26d8f596de1d1ba63f22f00fc7656fc15d97
superctj <congtj@umich.edu>
2024-06-25 03:28:19 -0400

    merge serialization logic to apply_text_template() method

--- observatory/preprocessing/tablewise.py
+++ observatory/preprocessing/tablewise.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import torch
 
-from observatory.preprocessing.columnwise import convert_table_to_col_list
-from observatory.preprocessing.rowwise import convert_table_to_row_list
+from transformers import PreTrainedTokenizer
+
 from observatory.preprocessing.preprocessing_wrapper import PreprocessingWrapper
 
 
@@ -35,11 +36,71 @@ def convert_table_to_str_rowwise(table: pd.DataFrame) -> str:
     return " ".join(rows)
 
 
-class TableMaxRowsPreprocessor(PreprocessingWrapper):
-    """Preprocessing for table embedding inference."""
+class TablewiseMaxRowsPreprocessor(PreprocessingWrapper):
+    """Preprocessing for table embedding inference of BERT-like model. The
+    preprocessor attempts to serialize each table columnwise or rowwise to a
+    sequence of tokens (up to the maximum number of rows that fit within the
+    model input size).
+    """
 
-    def is_fit(self, sample_table: pd.DataFrame) -> bool:
-        pass
+    def __init__(
+        self,
+        tokenizer: PreTrainedTokenizer,
+        max_input_size: int,
+        by_row: bool,
+        include_table_name: bool = True,
+        include_column_names: bool = True,
+    ):
+        super().__init__(tokenizer, max_input_size)
+
+        self.by_row = by_row
+        self.include_table_name = include_table_name
+        self.include_column_names = include_column_names
+
+    def apply_text_template(self, table: pd.DataFrame) -> list[str]:
+        """Convert a table to a list of columns or rows following a text
+        template
+
+        Args:
+            table:
+                A Pandas DataFrame representing a table.
+
+        Returns:
+            templated_table:
+                A list of column or row texts following the template.
+        """
+
+        if self.include_table_name:
+            templated_table = table.attrs.get("name")
+        else:
+            templated_table = ""
+
+        if self.by_row:
+            for row in table.itertuples(index=False):
+                if self.include_column_names:
+                    row_text = " ".join(
+                        [f"{col} {val}" for col, val in zip(table.columns, row)]
+                    )
+                else:
+                    row_text = " ".join([val for val in row])
+
+                if templated_table == "":
+                    templated_table = row_text
+                else:
+                    templated_table += f" {row_text}"
+        else:
+            for i in range(len(table.columns)):
+                col_text = " ".join(table.iloc[:, i].tolist())
+
+                if self.include_column_names:
+                    col_text = table.columns[i] + " " + col_text
+
+                if templated_table == "":
+                    templated_table = col_text
+                else:
+                    templated_table += f" {col_text}"
+
+        return templated_table
 
     def is_fit_columnwise(self, sample_table: pd.DataFrame) -> bool:
         """Check if a table fits within the maximum model input size based on
@@ -188,3 +249,6 @@ class TableMaxRowsPreprocessor(PreprocessingWrapper):
             truncated_tables.append(truncated_tbl)
 
         return truncated_tables
+
+    def truncate(self, table: pd.DataFrame):
+        pass
