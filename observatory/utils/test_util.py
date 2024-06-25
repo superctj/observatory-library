bf95c1d828a1c771c2a44a509a8999c624b1f77f
superctj <congtj@umich.edu>
2024-06-25 02:33:00 -0400

    add row embeddings assertion and revise docstrings

--- observatory/utils/test_util.py
+++ observatory/utils/test_util.py
@@ -6,8 +6,7 @@ def assert_num_embeddings_matches_number_columns(
     tables: list[pd.DataFrame],
     embeddings: torch.FloatTensor,
 ):
-    """Assert the number of embeddings matches the number of columns in each
-    table.
+    """Assert the number of embeddings matches the total number of columns.
 
     Args:
         tables:
@@ -22,3 +21,24 @@ def assert_num_embeddings_matches_number_columns(
         num_columns += tbl.shape[1]
 
     assert embeddings.shape[0] == num_columns
+
+
+def assert_num_embeddings_matches_number_rows(
+    cls_positions: list[list[int]],
+    embeddings: torch.FloatTensor,
+):
+    """Assert the number of embeddings matches the total number of rows.
+
+    Args:
+        cls_positions:
+            Lists of positions of [CLS] tokens.
+        embeddings:
+            Column embeddings of shape (<num_embeddings>, <embedding size>).
+    """
+
+    num_rows = 0
+
+    for cls_pos in cls_positions:
+        num_rows += len(cls_pos)
+
+    assert embeddings.shape[0] == num_rows
