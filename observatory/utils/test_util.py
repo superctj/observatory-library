fd42caf817b50ae44bc7faf4bcbaaaa93e9eb6de
superctj <congtj@umich.edu>
2024-07-06 01:42:24 -0400

    clean up unit test apis

--- observatory/utils/test_util.py
+++ observatory/utils/test_util.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 import pandas as pd
 import torch
 
@@ -33,46 +31,49 @@ def assert_num_embeddings_matches_num_columns(
 
 
 def assert_num_embeddings_matches_num_rows(
-    cls_positions: list[list[int]],
     embeddings: torch.FloatTensor,
+    num_rows: int = None,
+    cls_positions: list[list[int]] = None,
 ):
     """Assert the number of embeddings matches the total number of rows.
 
     Args:
-        cls_positions:
-            Lists of positions of [CLS] tokens.
         embeddings:
             Row embeddings of shape (<num_embeddings>, <embedding size>).
+        cls_positions:
+            Lists of positions of [CLS] tokens, each of which represents a row.
     """
 
-    num_rows = 0
+    if num_rows:
+        assert embeddings.shape[0] == num_rows
+    else:
+        assert cls_positions is not None
 
-    for cls_pos in cls_positions:
-        num_rows += len(cls_pos)
+        num_rows = 0
+        for cls_pos in cls_positions:
+            num_rows += len(cls_pos)
 
-    assert embeddings.shape[0] == num_rows
+        assert embeddings.shape[0] == num_rows
 
 
 def assert_num_embeddings_matches_num_tables(
-    tables: Union[list[pd.DataFrame], list[list[int]]],
-    embeddings: torch.FloatTensor,
+    embeddings: torch.FloatTensor, num_tables: int
 ):
     """Assert the number of embeddings matches the total number of tables.
 
     Args:
-        tables:
-            A list of tables or lists of positions of [CLS] tokens.
         embeddings:
             Table embeddings of shape (<num_embeddings>, <embedding size>).
+        num_tables:
+            Number of tables.
     """
 
-    assert embeddings.shape[0] == len(tables)
+    assert embeddings.shape[0] == num_tables
 
 
 def assert_num_embeddings_matches_num_cells(
     cell_embeddings: list[torch.FloatTensor],
-    tables: list[pd.DataFrame] = None,
-    cell_token_spans: list[list[tuple[int]]] = None,
+    cell_token_spans: list[list[tuple[int]]],
 ):
     """Assert the number of cell embeddings matches the number of cells in each
     table.
@@ -80,17 +81,9 @@ def assert_num_embeddings_matches_num_cells(
     Args:
         cell_embeddings:
             Cell embeddings of shape (<num_embeddings>, <embedding size>).
-        tables:
-            A list of tables.
         cell_token_spans:
             Lists of cell token spans per table.
     """
 
-    if tables:
-        for i, tbl in enumerate(tables):
-            assert cell_embeddings[i].shape[0] == tbl.size
-    else:
-        assert cell_token_spans is not None
-
-        for i, spans in enumerate(cell_token_spans):
-            assert cell_embeddings[i].shape[0] == len(spans)
+    for i, spans in enumerate(cell_token_spans):
+        assert cell_embeddings[i].shape[0] == len(spans)
